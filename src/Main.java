fe65d4260019dc9a105a364d0c5746ad8c5007da
Jiří Veselý <jurajplayercze@gmail.com>
2025-04-17 20:56:37 +0200

    Second homework init

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.time.LocalDate;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Executing program in first homework...");
+        System.out.println("Executing program in second homework...");
 
 
 
