9d1447016014e83c0db461f91e3bf203e99c62c7
Jiří Veselý <jurajplayercze@gmail.com>
2025-04-17 20:33:44 +0200

    Resetted template

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Executing program in first homework...");
 
-        Seller seller1 = new Seller("Honza", LocalDate.of(2005, 1, 5), 21, 61.1, true);
-        Seller seller2 = new Seller("Martin", LocalDate.of(2002, 1, 11), 7, 53.2, false);
 
-        System.out.println(seller1.name);
-        System.out.println(seller2.name);
+
     }
 }
