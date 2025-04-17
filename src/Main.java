76d60ed214f2d0c575cadbb67783352abbf741e4
Jiří Veselý <jurajplayercze@gmail.com>
2025-04-17 20:38:51 +0200

    Recreated simple mechanics for encapsulation

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,7 @@ public class Main {
         Seller seller1 = new Seller("Honza", LocalDate.of(2005, 1, 5), 21, 61.1, true);
         Seller seller2 = new Seller("Martin", LocalDate.of(2002, 1, 11), 7, 53.2, false);
 
-        System.out.println(seller1.name);
-        System.out.println(seller2.name);
+        System.out.println(seller1.getName());
+        System.out.println(seller2.getName());
     }
 }
--- src/Seller.java
+++ src/Seller.java
@@ -1,11 +1,11 @@
 import java.time.LocalDate;
 
 public class Seller {
-    String name;
-    LocalDate birthDate;
-    Integer contracts;
-    Double soldCarrotsInTons;
-    boolean senior;
+    private String name;
+    private LocalDate birthDate;
+    private Integer contracts;
+    private Double soldCarrotsInTons;
+    private boolean senior;
 
     Seller(){}
     Seller(String name, LocalDate birthDate, Integer contracts, Double soldCarrotsInTons, boolean senior){
@@ -13,6 +13,46 @@ public class Seller {
         this.birthDate = birthDate;
         this.contracts = contracts;
         this.soldCarrotsInTons = soldCarrotsInTons;
+        this.senior = senior;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getContracts() {
+        return contracts;
+    }
+
+    public void setContracts(Integer contracts) {
+        this.contracts = contracts;
+    }
+
+    public Double getSoldCarrotsInTons() {
+        return soldCarrotsInTons;
+    }
+
+    public void setSoldCarrotsInTons(Double soldCarrotsInTons) {
+        this.soldCarrotsInTons = soldCarrotsInTons;
+    }
+
+    public boolean isSenior() {
+        return senior;
+    }
+
+    public void setSenior(boolean senior) {
         this.senior = senior;
     }
 }
