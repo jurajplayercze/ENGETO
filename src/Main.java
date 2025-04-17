a21717f3fb996a81b192a951fc5cdf9f30a2c34b
Jiří Veselý <jurajplayercze@gmail.com>
2025-04-17 20:55:31 +0200

    Final cm

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,7 @@
 import java.time.LocalDate;
-
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Executing program in first homework...");
-
+        System.out.println("Executing program in second homework...");
         Seller seller1 = new Seller("Honza", LocalDate.of(2005, 1, 5), 21, 61.1, true);
         Seller seller2 = new Seller("Martin", LocalDate.of(2002, 1, 11), 7, 53.2, false);
 
